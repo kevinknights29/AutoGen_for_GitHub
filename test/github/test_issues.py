1969e2c023fc098618a9186dee463c9aad88f067
Kevin Knights <kevinknights29@hotmail.com>
2023-11-04 13:51:50 -0500

    fix: replaced indent with 4 spaces

--- test/github/test_issues.py
+++ test/github/test_issues.py
@@ -8,7 +8,7 @@ from src.github import issues
 def main():
     # test issues
     response = issues.get_issues()
-    print("Get Issues: ", json.dumps(response, indent="4"), sep="\n")
+    print("Get Issues: ", json.dumps(response, indent=" " * 4), sep="\n")
 
 
 if __name__ == "__main__":
